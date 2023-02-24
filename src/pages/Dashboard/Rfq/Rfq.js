180585f0b42d75037a3d74a174e0e10615a89052
Vaishnav <vaishnavmohan99@gmail.com>
2023-02-24 11:24:46 +0530

    Rfq without table

--- src/pages/Dashboard/Rfq/Rfq.js
+++ src/pages/Dashboard/Rfq/Rfq.js
@@ -6,44 +6,50 @@ import "./Rfq.css";
 const PurchaseOrderDashboard = () => {
 
   return (
+    
     <div className='container'>
+      <img className="asset-24x-1" alt="" src={require('../../images/logo.png')} />
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
     <div className='title'>
-        <h1>Purchase orders </h1>
+        <h1>RFQ </h1>
     </div>
     <br/>
 
-    <div className="row">
+    <div className="roww">
   
-          <div className="column">
-            <div className="card1">
-              <p>All</p>
-              <h2>1.345</h2>
+          <div className="columnn">
+            <div className="card4">
+              <p className="head1">All</p>
+              <h2 className="pp">1.345</h2>
             </div>
           </div>
 
-          <div className="column">
-            <div className="card1">
-            <p>Accepted</p>
-            <h2>2.890</h2>
+          <div className="columnn">
+            <div className="card4">
+            <p className="head1">Accepted</p>
+            <h2 className="pp">2.890</h2>
             </div>
           </div>
 
-          <div className="column">
-            <div className="card1">
-            <p>Pending</p>
-             <h2>$1.870</h2>
+          <div className="columnn">
+            <div className="card4">
+            <p className="head1">Pending</p>
+             <h2 className="pp">$1.870</h2>
             </div>
           </div>
 
-          <div className="column">
-            <div className="card2">
-            <p>Create New</p>
-            <p>Purchase Order</p>
+          <div className="columnn">
+            <div className="card5">
+            <p >Create New</p>
+            <p className="pp">RFQ</p>
             </div>
           </div>
      </div>
-  
-     <img className="asset-24x-1" alt="" src={require('../../images/logo.png')} />
 
 </div>
   );
